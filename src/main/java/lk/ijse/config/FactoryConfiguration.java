415efc9e27f86c5b13abea7213bc23aa2b094703
Menuki Peiris <139765897+menukipeiris@users.noreply.github.com>
2024-02-15 20:28:17 +0530

    Update FactoryConfiguration.java

--- src/main/java/lk/ijse/config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/config/FactoryConfiguration.java
@@ -1,8 +1,8 @@
-package org.example.config;
+package lk.ijse.config;
 
 
-import org.example.entity.Author;
-import org.example.entity.Book;
+import lk.ijse.entity.Author;
+import lk.ijse.entity.Book;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -11,16 +11,14 @@ public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
     private SessionFactory sessionFactory;
 
-    private FactoryConfiguration() {
-        Configuration configuration = (new Configuration()).configure().addAnnotatedClass(Author.class).addAnnotatedClass(Book.class);
-        this.sessionFactory = configuration.buildSessionFactory();
+    private FactoryConfiguration(){
+        Configuration configuration=new Configuration().configure().addAnnotatedClass(Book.class).addAnnotatedClass(Author.class);
+        sessionFactory=configuration.buildSessionFactory();
     }
-
-    public static FactoryConfiguration getFactoryConfiguration(){
-        return factoryConfiguration == null ? (factoryConfiguration = new FactoryConfiguration()) : factoryConfiguration;
+    public static FactoryConfiguration getInstance() {
+        return (factoryConfiguration==null)?factoryConfiguration=new FactoryConfiguration():factoryConfiguration;
     }
-
     public Session getSession(){
-        return this.sessionFactory.openSession();
+        return sessionFactory.openSession();
     }
 }
